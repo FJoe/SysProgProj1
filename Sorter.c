22951e7f136a00b844e7f68a610426817f5cbc25
Francis S. Joe <fsj6@design.cs.rutgers.edu>
2017-09-27 14:57:27 -0400

    Started sorter.h, created sorter.c, and made a makefile

---
+++ Sorter.c
@@ -0,0 +1,8 @@
+#include "Sorter.h"
+
+
+int main(int argc, char* argv[])
+{
+
+	return 0;
+}
--- Sorter.h
+++ Sorter.h
@@ -5,9 +5,19 @@
 *
 *
 ******/
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef SORTER_H
+#define SORTER_H
 
 //Suggestion: define a struct that mirrors a record (row) of the data set
-
+struct data_row{
+	int rowNum;
+	char* data;
+};
+typedef struct data_row DataRow;
 
 //Suggestion: prototype a mergesort function
 
+#endif
