6c8b9e5e4d6234fa31f29800471567c63c81b2ff
Peter C. Jeng <pcj20@singleton.cs.rutgers.edu>
2017-09-30 22:31:44 -0400

    Updated MergeSort to properly sort an array of integers Currently MergeSort is not an universal sorter

--- MergeSort.c
+++ MergeSort.c
@@ -3,62 +3,40 @@
 #include "Sorter.h"
 
 /*
-	REQUIRES DEBUGGING
-	Currently, the arraylist is not being sorted properly.
-	Testcase 1: {2, 1} sorts properly
-	Testcase 2: if |Size| > 2, then the program breaks down 
-	Could you type in the commands here for gdb? 
-
-	Forgot how to compile the program so we can run it in gdb
-
+	Currently can only sort integers
 */
 
 void merge(int * array, int left, int mid, int right)
 {
-	int tempLeftArraySize = mid - left;
+	int tempLeftArraySize = mid - left + 1;
 	int tempRightArraySize = right - mid;
-
-	//printf("%d, %d\n", tempLeftArraySize, tempRightArraySize);
 
 	//create temporary arrays, since we do not know how big these arrays will be, put them into heap to avoid stackoverflow
 	int * tempLeftArray = (int *)malloc(tempLeftArraySize * sizeof(int));
 	int * tempRightArray = (int *)malloc(tempRightArraySize * sizeof(int));
 
 	int i, j;
+	
 	//copy items into left array
 	for(i = 0; i < tempLeftArraySize; i++)
 	{	
 		//purpose of [left + j] is the mergesort can happen at different part of the original array
 		tempLeftArray[i] = array[left + i];
 	}
-	
-	/*
-	for(i = 0; i < tempLeftArraySize; i++){
-		printf("Left %d\n", tempLeftArray[i]);
-	
-	}
-	*/
-	
-	
+
 	//copy items into right array
-	
 	for(j = 0; j < tempRightArraySize; j++)
 	{
-		tempRightArray[j] = array[mid + j];
+		tempRightArray[j] = array[mid + j + 1];
 	}
 	
-	/*
-	for(j = 0; j < tempRightArraySize; j++){
-		printf("Right %d\n", tempRightArray[j]);
-	
-	}
-	*/	
 
 	//i is index of leftArray, j is index of rightArray, k is index of original array
 	i = 0;
 	j = 0;
-	int k = 0;
+	int k = left;
 
+    //comparison, compares left array with right array and the smaller value is put into original array
 	while(i < tempLeftArraySize && j < tempRightArraySize)
 	{
 		if(tempLeftArray[i] <= tempRightArray[j])
@@ -74,61 +52,38 @@ void merge(int * array, int left, int mid, int right)
 
 		k++;
 	}
-
-	while(i < tempLeftArraySize)
-	{
-		array[k] = tempLeftArray[i];
-		i++;
-		k++;
+	
+	//copy remaining elements in left array back into the original array
+	for(i; i < tempLeftArraySize; i++, k++){
+	    array[k] = tempLeftArray[i];
 	}
 
-	while(j < tempRightArraySize)
-	{
-		array[k] = tempRightArray[j];
-		j++;
-		k++;		
+    //copy remaining elements in right array back into the original array
+    for(j; j < tempRightArraySize; j++, k++){
+	    array[k] = tempRightArray[j];
 	}
+    
 
 	free(tempLeftArray);
 	free(tempRightArray);
 }
 
-void mergeSort(int array[], int left, int right)
+void mergeSort(int * array, int left, int right)
 {
 	int mid;
 
 	//if true, there is more than one element in the array
 	if(left < right)
 	{		
-		int arraySize =  sizeof(array)/sizeof(int);
-		int ptrsize = sizeof(array);
-		mid = (left + right) / 2;	
+		mid = (left + right) / 2;
+		
 		mergeSort(array, left, mid);
-
-		int i;
-
-
-		printf("Arraysize %d ptrsize %d\n", arraySize, ptrsize);
-
-		/*
-		for(i = 0; i < arraySize; i++){
-			printf("mergeSort: %d", array[i]);
-		}
-		printf("\n");
-		*/
-		
 		mergeSort(array, mid + 1, right);
-
-		/*
-		for(i = 0; i < arraySize; i++){
-			printf("mergeSort: %d", array[i]);
-		}
-
-		printf("\n");
-		*/
 		
-		printf("MID: %d\n", mid);
 		merge(array, left, mid, right);
+	}
+	else{
+	    return;
 	}
 }
  
@@ -137,13 +92,10 @@ void mergeSort(int array[], int left, int right)
 int main(int argc, char* argv[]){
 	//test
 
-	int array[] = {2, 1, 6, 8, 1 ,4 };
+	int array[] = {2, 1, 6, 4, 5, 3, 5, 7, 11, 2};
 	int arraySize = sizeof(array)/sizeof(int);
 
-	printf("array size: %d \n", arraySize);
-
 	mergeSort(array, 0, (arraySize - 1));
-	//merge(array, 0, arraySize/2, arraySize);
 	
 	int i;
 
@@ -156,5 +108,4 @@ int main(int argc, char* argv[]){
 
 	return 0;
 }
-
 
