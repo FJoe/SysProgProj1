2f339440a9406635ae91e38d0bc763aad7b926a9
Francis S. Joe <fsj6@design.cs.rutgers.edu>
2017-10-02 02:49:08 -0400

    Removed leading and trailing spaces in words. Still need to create array of data rows

--- Sorter.c
+++ Sorter.c
@@ -1,11 +1,85 @@
 #include "Sorter.h"
 
-/**
-void printRow(DataRow row)
-{
-	prin
+char* trimSpace(char* str){
+	int end = strlen(str) - 1;
+	while(str[end] == ' ' || str[end] == '\n')
+	{
+		str[end] = '\0';
+		end--;
+	}
+	
+
+	while(*str == ' ')
+	{
+		str++;
+	}
+
+	return str;
 }
-**/
+
+char getDataType(char* data){
+	if(strcmp(data, "color") == 0)
+		return 's';
+ 	else if(strcmp(data, "director_name") == 0)
+		return 's'; 
+	else if(strcmp(data, "actor_2_name") == 0)
+		return 's';
+	else if(strcmp(data, "genres") == 0)
+		return 's';
+	else if(strcmp(data, "actor_1_name") == 0)
+		return 's';
+	else if(strcmp(data, "movie_title") == 0)
+		return 's';
+	else if(strcmp(data, "actor_3_name") == 0)
+		return 's';
+	else if(strcmp(data, "plot_keywords") == 0)
+		return 's';
+	else if(strcmp(data, "movie_imdb_link") == 0)
+		return 's';
+	else if(strcmp(data, "language") == 0)
+		return 's';
+	else if(strcmp(data, "country") == 0)
+		return 's';
+	else if(strcmp(data, "content_rating") == 0)
+		return 's';
+
+	if(strcmp(data, "num_critic_for_reviews") == 0)
+		return 'n';
+	else if(strcmp(data, "duration") == 0)
+		return 'n'; 
+	else if(strcmp(data, "director_facebook_likes") == 0)
+		return 'n'; 
+	else if(strcmp(data, "actor_3_facebook_likes") == 0)
+		return 'n';
+	else if(strcmp(data, "actor_1_facebook_likes") == 0)
+		return 'n';
+	else if(strcmp(data, "gross") == 0)
+		return 'n';
+	else if(strcmp(data, "num_voted_users") == 0)
+		return 'n';
+	else if(strcmp(data, "cast_total_facebook_likes") == 0)
+		return 'n';
+	else if(strcmp(data, "facenumber_in_poster") == 0)
+		return 'n';
+	else if(strcmp(data, "num_user_for_reviews") == 0)
+		return 'n';
+	else if(strcmp(data, "budget") == 0)
+		return 'n';
+	else if(strcmp(data, "title_year") == 0)
+		return 'n';
+	else if(strcmp(data, "actor_2_facebook_likes") == 0)
+		return 'n';
+	else if(strcmp(data, "imdb_score") == 0)
+		return 'n';
+	else if(strcmp(data, "aspect_ratio") == 0)
+		return 'n';
+	else if(strcmp(data, "movie_facebook_likes") == 0)
+		return 'n';
+
+
+	return 'e';
+		
+}
 
 int main(int argc, char* argv[])
 {
@@ -24,15 +98,19 @@ int main(int argc, char* argv[])
 	}
 
 	//Create copy of column heading to print
-	char row1[1024];
-	char header[1024]; 
-	scanf("%1024[^\n]", row1);
-	strcpy(header, row1);
+	char* row1 = (char*) calloc(1024, sizeof(char));
+	if(fgets(row1, sizeof(char) * 1024, stdin) == NULL){
+		printf("ERROR: file empty");
+		return -1;
+	}
+
+	//Print column headers to output file
+	printf("%s", row1);
 
 	//If column to search for is found, get column number
 	char* colToSort = argv[2];
 	int colNumToSort = 0;
-	const char delim[2] = ", ";
+	const char delim[2] = ",";
 	char* curHead = strtok(row1, delim);
 
 	while( curHead != NULL && strcmp(colToSort, curHead) != 0){
@@ -44,9 +122,66 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 
-	//Print column headers to output file
-	printf("%s\n", header);
 
 	
+	char* origRow = (char*) calloc(1024, sizeof(char));
+
+	
+	//Goes through each row in csv file
+	while(fgets(origRow, sizeof(char) * 1024, stdin) != NULL){
+		//New dataRow for array
+		DataRow* newDataRow = (DataRow*) calloc(1, sizeof(DataRow));
+
+		//newRow is data for new row with trimmed spaces
+		//curRow is copy of orig csv file row
+		char* newRow = (char*) calloc(1024, sizeof(char));
+
+		char* curWord = strtok(origRow, delim);
+		int firstWord = 1;
+		int i = 0;
+		while(curWord != NULL){
+			if(firstWord)
+				firstWord = 0;
+			else
+				strcat(newRow, ",");
+
+			char* trimWord = trimSpace(curWord);
+
+			//DO LATER
+			/**
+			if(i == colNumToSort){
+				newDataRow->
+			}
+			**/
+
+			strcat(newRow, trimWord);
+			curWord = strtok(NULL, delim);
+
+			i++;
+		}
+		newDataRow->data = newRow;
+		printf("%s", newRow);
+		//free(newRow);
+	}
+	
+	free(row1);
+	free(origRow);
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Sorter.h
+++ Sorter.h
@@ -9,17 +9,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #ifndef SORTER_H
 #define SORTER_H
 
-//Suggestion: define a struct that mirrors a record (row) of the data set
+//data to compare each row by. Either comparing data of string or numeric values
+union data_compare{
+	double numData;
+	char* stringData;
+};
+typedef union data_compare DataCompare;
+
+
+//single row of csv file. Has content of the csv file, data to compare each row with, and the datatype of the data to compare with
 struct data_row{
-	int rowNum;
+	/**
+	dataType:
+		n = numData
+		s = stringData
+	**/
+	char dataType;
+	DataCompare dataCompare;	
 	char* data;
 };
 typedef struct data_row DataRow;
 
+//Trims leading and trailing spaces from a string
+char* trimSpace(char* str);
+
 //Suggestion: prototype a mergesort function
+
+
 
 #endif
